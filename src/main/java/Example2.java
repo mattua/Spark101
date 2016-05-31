57a5b903e2d4bfdfc758709988886b87a8827275
Matt Thomas <matt.thomas@markit.com>
2016-05-31 08:47:01 +0100

    boom

---
+++ src/main/java/Example2.java
@@ -0,0 +1,19 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Map;
+
+
+/**
+ * Created by matt.thomas on 23/05/2016.
+ */
+public class Example2 {
+
+}
